055e7e38213535f3447e259f78d2125b4c43226a
Gaspar <gabriel.sgaspar@yahoo.com.br>
2019-11-19 19:01:17 -0300

    Chapters 1 and 2, done.

---
+++ lib/kv/bucket.ex
@@ -0,0 +1,42 @@
+defmodule KV.Bucket do
+    @moduledoc """
+    Ao se interagir com a API do módulo Agent, não é interessante espalhar as funções do módulo ao longo dos arquivos do app. Então, uma boa prática é centralizar tudo em um módulo (costuma-se chamar ele de "Bucket"), e usar as funções desse módulo ao longo do app. 
+
+    Uma coisa que se deve atentar é que, ao criar uma instância do Agent, criamos um processo que vai rodar em loop e armazenar as informações que queremos persistir.
+    Quando interagimos com a API do Agent, estamos, na verdade, enviando e recebendo mensagens para o processo que está rodando em loop. Dizemos que esse processo atua como servidor. 
+    A função anônima que passamos nas chamadas das API são executadas no lado do servidor. Isso significa que se você colocar muitas operações dentro da função anônima, o processo vai ficar ocupado com ela e só vai atender requisições de outros clientes após ter terminado todas as operações. Enquanto ele processa, os outros clientes ficam aguardando e podem acabar entrando em timeout.
+    Por isso, seja sábio ao definir o que vai ser executado no lado do servidor (dentro da função anônima) e o que vai ser executado no lado cliente (fora da função anônima, isto é, antes de se fazer a chamada da API).
+    """
+    use Agent
+
+    @doc """
+    Starts a new bucket.
+    _opts é uma lista de parâmetros opcionais que podem ser passados para a função start_link, de modo a influenciar alguma operação na hora de criar o estado.
+    """
+    def start_link(_opts) do
+        Agent.start_link(fn -> %{} end)
+    end
+
+    @doc """
+    Obter um valor do "Bucket" a partir de uma "key"
+    """
+    def get(bucket, key) do
+        Agent.get(bucket, fn bucket -> Map.get(bucket, key) end )
+    end
+
+    @doc """
+    Inserir um valor, identificado por uma "key", dentro do "Bucket"
+    """
+    def put(bucket, key, value) do
+        Agent.update(bucket, fn bucket -> Map.put(bucket, key, value) end )
+    end
+
+    @doc """
+    Deletar um valor do "bucket", a partir de uma "key"
+    """
+    def delete(bucket, key) do
+        Agent.get_and_update(bucket, fn bucket -> Map.pop(bucket, key) end)
+    end
+
+
+end
---
+++ test/kv/bucket_test.exs
@@ -0,0 +1,25 @@
+defmodule KV.BucketTest do
+    # A cláusula "use" permite ao módulo ExUnit.Case inserir códigos e funcionalidades neste módulo. Isso é desejado, já que queremos que este módulo seja usado nas rotinas de testes, então ele precisa ser usado pelo módulo ExUnit.Case.
+    # Foi passado como argumento async: true, o que significa que os testes definidos aqui rodarão em paralelo com outras rotinas de testes. Isso é muito útil quando há várias rotinas de testes a serem executadas.
+    # OBS: Caso um teste dependa de outro para acontecer, ou de alguma operação, como leitura/escrita em banco de dados, não se deve usar o argumento async.
+    use ExUnit.Case, async: true
+
+    # Essa macro é uma callback que é chamada antes de cada um dos testes definidos neste módulo. Ela basicamente cria um bucket e o retorna dentro de um mapa.
+    # Esse mapa é passado como argumento para os testes, que podem acessá-lo a partir do mecanismo de "pattern matching".
+    setup do
+        {:ok, bucket} = KV.Bucket.start_link([])
+        %{bucket: bucket}
+    end
+
+    test "stores values by key", %{bucket: bucket} do
+        assert KV.Bucket.get(bucket, "milk") == nil
+
+        KV.Bucket.put(bucket, "milk", 3)
+        assert KV.Bucket.get(bucket, "milk") == 3
+    end
+
+    test "deletes a value by key", %{bucket: bucket} do
+        KV.Bucket.delete(bucket, "milk")
+        assert KV.Bucket.get(bucket, "milk") == nil
+    end
+end
