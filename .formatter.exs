4e054fb27f3e1d10e2541ddbb5a0139818bd33a6
Gaspar <gabriel.sgaspar@yahoo.com.br>
2019-11-19 16:30:26 -0300

    First commit, setting up the project

---
+++ .formatter.exs
@@ -0,0 +1,5 @@
+# Used by "mix format"
+# Esse comando vai formatar os arquivos com as extensões definidas aqui, removendo espaçamentos e quebras de linhas extras do código, tudo seguindo um padrão configurável. 
+[
+  inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"]
+]
